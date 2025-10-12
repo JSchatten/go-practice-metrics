af745aa6ea6ff1c120f3dd63c7aedafd03b8a346
Иван Щербаков-Щекало <isshcherbakov-shchekalo@sberads.tech>
2025-10-12 15:47:59 +0300

    Forgotten changes in server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,9 +15,12 @@ type Storage interface {
 
 func main() {
 	storageObj := storage.NewMemStorage()
-	http.HandleFunc("/update/", handlers.UpdateHandler(storageObj))
 	// http.HandleFunc("/live/", handlers.LiveHandler())
 	// http.HandleFunc("/", handlers.LiveHandler())
+	http.HandleFunc("/update/", handlers.UpdateHandler(storageObj))
+	http.HandleFunc("/value/", handlers.ValueHandler(storageObj))
+	http.HandleFunc("/", handlers.RootHandler(storageObj))
+
 	fmt.Println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
